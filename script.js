b6797c00e9cfa6d7f1b5a96835232c5b4ac17125
Harshit Arora <harshitarora0116@gmail.com>
2026-06-25 20:50:59 +0530

    brush is working, buttons have some bugs in toggling simultaneously, eraser yet to be implemented, custom grid size yet to be implemented

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
 const canvas = document.querySelector('.canvas');
+let isDrawing = false;
+let isBrush = false;
+let isEraser = false;
 
 for (let i = 0; i < 16 * 16; i ++){
     const square = document.createElement('div');
     square.classList.add('grid-square');
     canvas.appendChild(square);
+    square.addEventListener('mouseenter', ()=> {
+        if (isDrawing && isBrush) {
+            square.style.backgroundColor = 'black';
+        }
+    })
+    if (isBrush){
+    square.addEventListener('mousedown', () => {
+        square.style.backgroundColor = 'black';
+    })}
 }
+
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+
+const brush = document.querySelector('.brush');
+brush.addEventListener('click', toggleBrush
+)
+
+function toggleBrush(){
+    isBrush = !isBrush;
+    if (isBrush){
+    brush.style.backgroundColor = 'green';}
+    else{
+        brush.style.backgroundColor = '#9023dc';
+    }
+    if (isEraser){
+        toggleEraser();
+    }
+    }
+
+function toggleEraser(){
+    isEraser = !isEraser;
+    if (isEraser){
+    eraser.style.backgroundColor = 'green';}
+    else{
+        eraser.style.backgroundColor = '#9023dc';
+    }
+}
+
+const eraser = document.querySelector('.eraser')
+eraser.addEventListener('click', toggleEraser)
